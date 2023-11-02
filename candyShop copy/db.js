c34eff738860c009246b423bfd33fff2abeabcce
Ankit kumar <smpratik888@gmail.com>
2023-11-02 13:40:59 +0530

    Delete candyShop copy directory

--- candyShop copy/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const db = new Sequelize("candy_shop_db", "root", "Ankit@123", {
-    host: "localhost",
-    dialect: "mysql"
-});
-
-module.exports = db;
